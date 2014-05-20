3a75b93754c4d7744244a8352ce1e3307392811d
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2014-05-20 17:43:46 +0000

    Changed how preconditioning is handled in L-BFGS
    Now using the preconditioner as a metric (consistent with CG),
    rather than as an initial Hessian (in the original BFGS papers).
    This allows caching of the preconditioned gradient, since the
    preconditioner is always applied to the gradient at a point on the
    minimization trajectory. This is a must for using L-BFGS with the
    electronic system, where precondition is not simply a matrix multiply
    due to the nontrivial handling of  fillings / unoccupied state gradients.

--- jdftx/core/Minimize_lBFGS.h
+++ jdftx/core/Minimize_lBFGS.h
@@ -31,6 +31,8 @@ template<typename Vector> double Minimizable<Vector>::lBFGS(const MinimizeParams
 {	
 	Vector g; //gradient
 	double E = sync(compute(&g)); //get initial energy and gradient
+	Vector Kg = precondition(g); //preconditioned gradient
+	
 	EdiffCheck ediffCheck(p.nEnergyDiff, p.energyDiffThreshold); //list of past energies
 
 	double alphaT = p.alphaTstart; //test step size
@@ -40,11 +42,11 @@ template<typename Vector> double Minimizable<Vector>::lBFGS(const MinimizeParams
 	//History of variable and residual changes:
 	struct History
 	{	Vector s; //change in variable (= alpha d)
-		Vector y; //change in residual (= g - gPrev)
+		Vector Ky; //change in preconditioned residual (= Kg - KgPrev)
 		double rho; //= 1/dot(s,y)
-		double gamma; //= dot(s,y)/dot(y,y)
 	};
 	std::list< std::shared_ptr<History> > history;
+	double gamma = 0.; //scaling: set to dot(s,y)/dot(y,Ky) at end of first iteration
 	
 	//Select the linmin method:
 	Linmin linmin = getLinmin(p);
@@ -55,20 +57,21 @@ template<typename Vector> double Minimizable<Vector>::lBFGS(const MinimizeParams
 	{	
 		if(report(iter)) //optional reporting/processing
 		{	E = sync(compute(&g)); //update energy and gradient if state was modified
+			Kg = precondition(g);
 			fprintf(p.fpLog, "%s\tState modified externally: resetting history.\n", p.linePrefix);
 			fflush(p.fpLog);
 			history.clear();
 		}
 		
-		double gNorm = sync(dot(g,g));
-		fprintf(p.fpLog, "%sIter: %3d  %s: %22.15le  |grad|: %10.3le  alpha: %10.3le",
-			p.linePrefix, iter, p.energyLabel, E, sqrt(gNorm/p.nDim), alpha);
+		double gKnorm = sync(dot(g,Kg));
+		fprintf(p.fpLog, "%sIter: %3d  %s: %22.15le  |grad|_K: %10.3le  alpha: %10.3le",
+			p.linePrefix, iter, p.energyLabel, E, sqrt(gKnorm/p.nDim), alpha);
 		if(linminTest) fprintf(p.fpLog, ", linmin = %10.3le", linminTest);
 		
 		//Check stopping conditions:
 		fprintf(p.fpLog, "\n"); fflush(p.fpLog);
-		if(sqrt(gNorm/p.nDim) < p.knormThreshold)
-		{	fprintf(p.fpLog, "%sConverged (|grad|<%le).\n", p.linePrefix, p.knormThreshold);
+		if(sqrt(gKnorm/p.nDim) < p.knormThreshold)
+		{	fprintf(p.fpLog, "%sConverged (|grad|_K<%le).\n", p.linePrefix, p.knormThreshold);
 			fflush(p.fpLog); return E;
 		}
 		if(ediffCheck.checkConvergence(E))
@@ -76,8 +79,8 @@ template<typename Vector> double Minimizable<Vector>::lBFGS(const MinimizeParams
 				p.linePrefix, p.energyLabel, p.energyDiffThreshold, p.nEnergyDiff);
 			fflush(p.fpLog); return E;
 		}
-		if(!std::isfinite(gNorm))
-		{	fprintf(p.fpLog, "%s|grad|=%le. Stopping ...\n", p.linePrefix, gNorm);
+		if(!std::isfinite(gKnorm))
+		{	fprintf(p.fpLog, "%s|grad|_K=%le. Stopping ...\n", p.linePrefix, gKnorm);
 			fflush(p.fpLog); return E;
 		}
 		if(!std::isfinite(E))
@@ -88,28 +91,26 @@ template<typename Vector> double Minimizable<Vector>::lBFGS(const MinimizeParams
 		
 		//Prepare container that will be committed to history below: (and avoid copies)
 		auto h = std::make_shared<History>();
-		Vector& gPrev = h->y;
 		Vector& d = h->s;
-		gPrev = clone(g);
 		
 		//Compute search direction:
-		d = clone(g);
+		d = clone(Kg);
 		std::stack<double> a; //alpha in the reference renamed to 'a' here to not clash with step size
 		for(auto h=history.rbegin(); h!=history.rend(); h++)
 		{	a.push( (*h)->rho * sync(dot((*h)->s, d)) );
-			axpy(-a.top(), (*h)->y, d);
+			axpy(-a.top(), (*h)->Ky, d);
 		}
-		d = precondition(d);
-		if(history.size()) //H0 = K at first iteration, but incorporate scaling on subsequent ones (scheme M3 from the paper)
-			d *= history.back()->gamma;
+		if(gamma) d *= gamma; //scaling (available after first iteration)
 		for(auto h=history.begin(); h!=history.end(); h++)
-		{	double b = (*h)->rho * sync(dot((*h)->y, d));
+		{	double b = (*h)->rho * sync(dot((*h)->Ky, d));
 			axpy(a.top()-b, (*h)->s, d);
 			a.pop();
 		}
 		d *= -1;
+		if((int)history.size() == p.history) history.pop_front(); //if full, make room for the upcoming history entry (do this here to free memory earlier)
 		
 		//Line minimization
+		Vector y = clone(g); //store previous gradient before linmin changes it (this will later be converted to y = g-gPrev)
 		if(linmin(*this, p, d, alphaT, alpha, E, g))
 		{	//linmin succeeded:
 			if(p.updateTestStepSize)
@@ -124,11 +125,13 @@ template<typename Vector> double Minimizable<Vector>::lBFGS(const MinimizeParams
 			fprintf(p.fpLog, "%s\tUndoing step.\n", p.linePrefix);
 			step(d, -alpha);
 			E = sync(compute(&g));
+			Kg = precondition(g);
 			if(history.size())
 			{	//Failed, but not along the gradient direction:
 				fprintf(p.fpLog, "%s\tStep failed: resetting history.\n", p.linePrefix);
 				fflush(p.fpLog);
 				history.clear();
+				gamma = 0.;
 				linminTest = 0.;
 				continue;
 			}
@@ -144,12 +147,13 @@ template<typename Vector> double Minimizable<Vector>::lBFGS(const MinimizeParams
 		//Update history:
 		linminTest = sync(dot(g,d))/sqrt(sync(dot(g,g))*sync(dot(d,d)));
 		d *= alpha; //d -> alpha * d, which is change of state, and it resides in h->s
-		gPrev *= -1.; axpy(1., g, gPrev); //gPrev -> g - gPrev, and it resides in h->y
-		double ydots = sync(dot(h->y, h->s));
+		h->Ky = Kg; Kg = precondition(g); //store previous preconditioned gradient, and compute new one
+		h->Ky *= -1; axpy(1., Kg, h->Ky); //Ky = K(g-gPrev)
+		y *= -1; axpy(1., g, y); //y = g-gPrev
+		double ydots = sync(dot(y, h->s));
 		h->rho = 1./ydots;
-		h->gamma = ydots / sync(dot(h->y, precondition(h->y)));
+		if(!gamma) gamma = ydots / sync(dot(y, h->Ky));
 		history.push_back(h);
-		while((int)history.size() > p.history) history.pop_front(); //dicard oldest entries
 	}
 	fprintf(p.fpLog, "%sNone of the convergence criteria satisfied after %d iterations.\n", p.linePrefix, iter);
 	return E;
