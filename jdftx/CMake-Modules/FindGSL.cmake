7cca2b5f955a4ea604640ac70e967ebba1ee0843
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2014-06-11 21:05:50 +0000

    Added versionc hecking to FindGSL.cmake

--- jdftx/CMake-Modules/FindGSL.cmake
+++ jdftx/CMake-Modules/FindGSL.cmake
@@ -1,7 +1,17 @@
 find_path(GSL_INCLUDE_DIR gsl/gsl_cblas.h ${GSL_PATH} ${GSL_PATH}/include /usr/include /usr/local/include)
 find_library(GSL_LIBRARY NAMES gsl PATHS ${GSL_PATH} ${GSL_PATH}/lib ${GSL_PATH}/lib64 /usr/lib /usr/lib64 /usr/local/lib /usr/local/lib64)
 
+#Check version:
 if(GSL_INCLUDE_DIR AND GSL_LIBRARY)
+	include(CheckSymbolExists)
+	find_library(GSL_BLAS_LIBRARY NAMES gslcblas PATHS ${GSL_PATH} ${GSL_PATH}/lib ${GSL_PATH}/lib64 /usr/lib /usr/lib64 /usr/local/lib /usr/local/lib64)
+	if(GSL_BLAS_LIBRARY)
+		set(CMAKE_REQUIRED_LIBRARIES "${GSL_LIBRARY};${GSL_BLAS_LIBRARY};${CMAKE_REQUIRED_LIBRARIES}")
+		check_symbol_exists("gsl_integration_glfixed_point" "${GSL_INCLUDE_DIR}/gsl/gsl_integration.h" GSL_VERSION_ADEQUATE)
+	endif()
+endif()
+
+if(GSL_INCLUDE_DIR AND GSL_LIBRARY AND GSL_VERSION_ADEQUATE)
 	set(GSL_FOUND TRUE)
 endif()
 
@@ -17,6 +27,9 @@ else()
 		endif()
 		if(NOT GSL_LIBRARY)
 			message(FATAL_ERROR "Could not find the GNU Scientific Library shared libraries (Add -D GSL_PATH=<path> to the cmake commandline for a non-standard installation)")
+		endif()
+		if(NOT GSL_VERSION_ADEQUATE)
+			message(FATAL_ERROR "Unsupported GNU Scientific Library version; need version >= 1.15")
 		endif()
 	endif()
 endif()
