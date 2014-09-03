cb3e494d29b2dd9f7a7e6cee32ff168d3a9b6093
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2014-09-03 17:12:02 +0000

    Freezing GBRV pseudopotential library version (including static tarball on svn with permission)

--- jdftx/pseudopotentials/update.sh
+++ jdftx/pseudopotentials/update.sh
@@ -4,17 +4,5 @@ DEST_DIR="$1"
 SRC_DIR="$2"
 
 #------ GBRV pseudoptentials ------
-GBRV_PATH="$DEST_DIR/GBRV"
-
-echo "Downloading GBRV pseudopotentials from http://www.physics.rutgers.edu/gbrv to $GBRV_PATH
-     K.F. Garrity, J.W. Bennett, K.M. Rabe and D. Vanderbilt, Comput. Mater. Sci. 81, 446 (2014)
-These pseudopotentials are distributed under the GPLv3 license. Please see the
-above website for copyright information as well as details on the pseudopotentials."
-
-mkdir -p $GBRV_PATH
-function downloadGBRV()
-{	wget --timestamping --max-redirect=0 --no-verbose --directory-prefix=$GBRV_PATH http://www.physics.rutgers.edu/gbrv/$1
-	tar -C $GBRV_PATH -xpzf $GBRV_PATH/$1
-}
-downloadGBRV all_pbe_uspp.tar.gz
-downloadGBRV all_lda_uspp.tar.gz
+rm -rf $DEST_DIR/GBRV #Cleanup existing
+tar -C $DEST_DIR -xpzf $SRC_DIR/GBRV.tgz
