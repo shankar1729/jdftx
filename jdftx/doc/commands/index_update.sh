bc97278deb02d3d61059167cb92c2534c2cdbddc
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2015-03-25 15:31:54 +0000

    Moved the autogenerated dox files from the source to the build directory (and hence off version control)

--- jdftx/doc/commands/index_update.sh
+++ jdftx/doc/commands/index_update.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 srcPath="$1"
-outFile="$2"
 
-echo '/** \page CommandIndex Index of commands' > $outFile
-awk '$1=="/**" && $2=="\\page" {print "+ \\subpage ", $3}' $srcPath/*/manual.dox | sort >> $outFile
-echo '*/' >> $outFile
+echo '/** \page CommandIndex Index of commands'
+awk '$1=="/**" && $2=="\\page" {print "+ \\subpage ", $3}' $srcPath/commands/*.dox | sort
+echo '*/'
