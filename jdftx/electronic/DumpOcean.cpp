7df51172a6da93d60bef8a7777168c3580a4c021
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2015-03-31 19:32:54 +0000

    Fixed k-point wrapping convention in OCEAN output

--- jdftx/electronic/DumpOcean.cpp
+++ jdftx/electronic/DumpOcean.cpp
@@ -23,8 +23,10 @@ along with JDFTx.  If not, see <http://www.gnu.org/licenses/>.
 #include <electronic/matrix.h>
 #include <electronic/operators.h>
 #include <electronic/ElecMinimizer.h>
+#include <electronic/ColumnBundleTransform.h>
 #include <core/Thread.h>
 #include <core/Operators.h>
+#include <core/LatticeUtils.h>
 #include <config.h>
 #include <map>
 
@@ -54,41 +56,70 @@ void Dump::dumpOcean()
 		else //All the writing happens from head
 		{
 			//Open a file for each k-point:			
-			std::string fname = "kpoint" + std::to_string (ik);
+			std::string fname = "kpoint" + std::to_string(ik);
 			logPrintf("\tDumping '%s' ... ", fname.c_str()); logFlush();
 			FILE *fp = fopen(fname.c_str(), "wb");
 			if(!fp) die_alone("Error opening %s for writing.\n", fname.c_str());
 			
+			//Initialize k-point transformation from JDFTx to OCEAN convention:
+			const Basis& basis = e->basis[ik];
+			const QuantumNumber& qnum = eInfo.qnums[ik];
+			//--- quantum number
+			QuantumNumber qnumOut = qnum;
+			logPrintf("at k = [ ");
+			for(int j=0; j<3; j++)
+			{	qnumOut.k[j] -= floor(qnumOut.k[j]); //wrap to [0,1)
+				if(fabs(qnumOut.k[j] - 1.) < symmThreshold)
+					qnumOut.k[j] -= 1.; //make sure components ~ 0.9999 wrap to 0
+				logPrintf("%+.6f ", qnumOut.k[j]);
+			}
+			logPrintf("] "); logFlush();
+			//--- basis
+			Basis basisOut;
+			{	const GridInfo& gInfoBasis = e->gInfoWfns ? *(e->gInfoWfns) : e->gInfo;
+				const vector3<> kbasis = (e->cntrl.basisKdep==BasisKpointDep) ? qnumOut.k : vector3<>();
+				logSuspend();
+				basisOut.setup(gInfoBasis, e->iInfo, e->cntrl.Ecut, kbasis);
+				logResume();
+			}
+			//--- transformation indices
+			ColumnBundleTransform transform(qnum.k, basis, qnumOut.k, basisOut, 1, matrix3<int>(1,1,1), +1);
+			
 			//Write header:
-			int nbasis = e->basis[ik].nbasis; //cast nbasis to int
+			int nbasis = basisOut.nbasis; //cast nbasis to int
 			fwrite(&nbasis, sizeof(int), 1, fp); //Number of G-vectors
-			fwrite(e->basis[ik].iGarr, sizeof(int), 3*nbasis, fp); //List of G-vectors
+			fwrite(basisOut.iGarr, sizeof(int), 3*nbasis, fp); //List of G-vectors
 			
 			//Collect relevant wavefunctions:
-			std::vector<ColumnBundle> CkTemp(nSpins);
-			std::vector<const ColumnBundle*> Ck(nSpins);
+			std::vector<ColumnBundle> Ck(nSpins);
 			for(int s=0; s<nSpins; s++)
 			{	int q = ik + nkPoints*s; //net quantum number
+				//Get wavefunction at JDFTx k-point:
+				ColumnBundle CkTemp;
+				const ColumnBundle* CkIn;
 				if(eInfo.isMine(q))
-					Ck[s] = &eVars.C[q];
+					CkIn = &eVars.C[q];
 				else
-				{	Ck[s] = &CkTemp[s];
-					CkTemp[s].init(eInfo.nBands, e->basis[q].nbasis, &e->basis[q], &eInfo.qnums[q]);
-					CkTemp[s].recv(eInfo.whose(q), s);
+				{	CkIn = &CkTemp;
+					CkTemp.init(eInfo.nBands, nbasis, &basis, &qnum);
+					CkTemp.recv(eInfo.whose(q), s);
 				}
+				//Convert to OCEAN k-point (and add normalization factor):
+				Ck[s].init(eInfo.nBands, nbasis, &basisOut, &qnumOut);
+				Ck[s].zero();
+				transform.scatterAxpy(sqrtvol, *CkIn, Ck[s], 0, 1);
 			}
 			
 			//Write wavefunctions:
 			std::vector<complex> phases(2);
-			phases[0] = complex(+sqrtvol, 0); //normalize and get real part below
-			phases[1] = complex(0, -sqrtvol); //normalize and get imag part below
+			phases[0] = complex(+1, 0); //get real part below
+			phases[1] = complex(0, -1); //get imag part below
 			for(complex phase: phases) //loop over real/imag part
-				for(const ColumnBundle* Cq: Ck) //loop over spin
-					(phase * (*Cq)).write_real(fp); //outer loop over bands, inner loop over G-vectors
+				for(const ColumnBundle& Cq: Ck) //loop over spin
+					(phase * Cq).write_real(fp); //outer loop over bands, inner loop over G-vectors
 			
 			fclose(fp);
 			logPrintf("done.\n"); logFlush();
 		}
 	}
-	
 }
