7e87d5e2828598a74ec712c3869468eef6a590f1
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2015-07-14 00:34:54 +0000

    Adding SG15 norm-conserving pseudopotential set to distribution

--- jdftx/pseudopotentials/update.sh
+++ jdftx/pseudopotentials/update.sh
@@ -6,3 +6,7 @@ SRC_DIR="$2"
 #------ GBRV pseudoptentials ------
 rm -rf $DEST_DIR/GBRV #Cleanup existing
 tar -C $DEST_DIR -xpzf $SRC_DIR/GBRV.tgz
+
+#------ SG15 pseudoptentials ------
+rm -rf $DEST_DIR/SG15 #Cleanup existing
+tar -C $DEST_DIR -xpzf $SRC_DIR/SG15.tgz
