cb1d961683bfa55d1c025a620b017f1868c63a6d
Ravishankar Sundararaman <shankars@caltech.edu>
2016-02-07 13:22:13 -0800

    Updating script documentation generation to work even if scripts not in path; adding intro para to the resulting page

--- jdftx/doc/scripts/index_update.sh
+++ jdftx/doc/scripts/index_update.sh
@@ -5,7 +5,13 @@ cd "$srcPath/scripts"
 
 scriptList=( ` find . -maxdepth 1 -type f -printf '%f\n' ` ) #get only files (ignore directories)
 
-echo '/** \page Scripts Scripts'
+echo '/** \page Scripts Scripts
+
+JDFTx includes several scripts to ease input generation and process / visualize output.
+These scripts are located in the jdftx/scripts source directory.
+Please add this directory to your PATH variable for convenience.
+
+'
 for t in "${scriptList[@]}"; do
 	echo " + \subpage " $t
 done
@@ -15,7 +21,7 @@ echo
 for t in "${scriptList[@]}"; do
 	echo '/** \page ' $t $t
 	echo "-----------------"
-	$t --help
+	./$t --help
 	echo '*/'
 	echo
 done
