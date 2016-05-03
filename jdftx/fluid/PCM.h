eb0a66d7134fbb0fe58010e3ca8c6cfd6acff314
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2016-05-03 06:04:09 -0700

    Fixed infinite recursion warnings (and a few other minor ones) in Clang 3.8

--- jdftx/fluid/PCM.h
+++ jdftx/fluid/PCM.h
@@ -49,7 +49,7 @@ protected:
 	ScalarFieldTilde getFullCore() const; //!< get full core correction for PCM variants that need them
 private:
 	ScalarField Acavity_shape, Acavity_shapeVdw; //!< Cached gradients of cavitation (and dispersion) energies w.r.t shape functions
-	double A_nc, A_tension, A_vdwScale, A_eta_wDiel, A_pCavity, A_T0, A_T1; //!< Cached derivatives w.r.t fit parameters (accessed via dumpDebug() for PCM fits)
+	double A_nc, A_tension, A_vdwScale, A_eta_wDiel, A_pCavity; //!< Cached derivatives w.r.t fit parameters (accessed via dumpDebug() for PCM fits)
 	double Rex[2]; //!< radii for cavity expansion (SGA13 only)
 	RadialFunctionG wExpand[2]; //!< weight function for cavity expansion (SGA13 only)
 	RadialFunctionG wCavity; //!< weight function for nonlocal cavitation energy
