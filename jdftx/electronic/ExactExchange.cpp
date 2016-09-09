cc64bbf0198575bd2578c8ddad849fa383dce3ea
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2016-09-08 22:36:15 -0400

    Fixed subtle symmetry issue in new EXX implementation; works with MPI and noncollinear correctly now.

--- jdftx/electronic/ExactExchange.cpp
+++ jdftx/electronic/ExactExchange.cpp
@@ -35,22 +35,25 @@ public:
 	ExactExchangeEval(const Everything& e);
 	
 	//! Calculate for one entry of the k-mesh at a particular spin:
-	double calc(int iSpin, int ik, double aXX, double omega, const std::vector<diagMatrix>& F, const std::vector<ColumnBundle>& C, std::vector<ColumnBundle>* HC) const;
+	double calc(int iSpin, unsigned iReduced, unsigned iInvert, unsigned iSym, 
+		double aXX, double omega, const std::vector<diagMatrix>& F, const std::vector<ColumnBundle>& C, std::vector<ColumnBundle>* HC) const;
 	
 private:
 	friend class ExactExchange;
 	const Everything& e;
 	const std::vector< matrix3<int> >& sym; //!< symmetry matrices in lattice coordinates
+	const std::vector<int>& invertList; //!< whether to add inversion explicitly over the symmetry group
 	int nSpins;
 	int nSpinor;
 	int qCount; //!< number of states of each spin
-	//Full kMesh properties:
-	struct KmeshEntry : public Supercell::KmeshTransform
+	//Symmetry rotation map:
+	struct KmapEntry
 	{	vector3<> k;
 		Basis basis;
 		std::shared_ptr<ColumnBundleTransform> transform; //wavefunction transformation from reduced set
 	};
-	std::vector<KmeshEntry> kmesh;
+	std::vector<KmapEntry> kmap;
+	inline int kmapIndex(int iReduced, int iInvert, int iSym) const { return (iReduced*invertList.size() + iInvert)*sym.size() + iSym; }
 };
 
 
@@ -81,8 +84,10 @@ double ExactExchange::operator()(double aXX, double omega,
 	//Calculate:
 	double EXX = 0.0;
 	for(int iSpin=0; iSpin<eval->nSpins; iSpin++)
-		for(unsigned ik=0; ik<eval->kmesh.size(); ik++)
-			EXX += eval->calc(iSpin, ik, aXX, omega, F, C, HC);
+		for(int iReduced=0; iReduced<eval->qCount; iReduced++)
+		for(unsigned iInvert=0; iInvert<eval->invertList.size(); iInvert++)
+		for(unsigned iSym=0; iSym<eval->sym.size(); iSym++)
+			EXX += eval->calc(iSpin, iReduced, iInvert, iSym, aXX, omega, F, C, HC);
 	watch.stop();
 	return EXX;
 }
@@ -94,43 +99,49 @@ double ExactExchange::operator()(double aXX, double omega,
 ExactExchangeEval::ExactExchangeEval(const Everything& e)
 : e(e),
 	sym(e.symm.getMatrices()),
+	invertList(e.symm.getKpointInvertList()),
 	nSpins(e.eInfo.spinType==SpinZ ? 2 : 1),
 	nSpinor(e.eInfo.spinorLength()),
-	qCount(e.eInfo.nStates/nSpins)
+	qCount(e.eInfo.nStates/nSpins),
+	kmap(qCount * invertList.size() * sym.size())
 {
 	//Print cost estimate to give the user some idea of how long it might take!
 	double costFFT = e.eInfo.nStates * e.eInfo.nBands * 9.*e.gInfo.nr*log(e.gInfo.nr);
 	double costBLAS3 = e.eInfo.nStates * pow(e.eInfo.nBands,2) * e.basis[0].nbasis;
 	double costSemiLocal = 8 * costBLAS3 + 3 * costFFT; //very rough estimates of course!
-	double costEXX = costFFT * ( e.coulombParams.supercell->kmesh.size() * e.eInfo.nBands );
+	double costEXX = costFFT * ( sym.size() * invertList.size() * qCount * e.eInfo.nBands );
 	double relativeCost = 1+costEXX/costSemiLocal;
 	double relativeCostOrder = pow(10, floor(log(relativeCost)/log(10)));
 	relativeCost = round(relativeCost/relativeCostOrder) * relativeCostOrder; //eliminate extra sigfigs
 	logPrintf("Per-iteration cost relative to semi-local calculation ~ %lg\n", relativeCost);
+	if(qCount==1 && sym.size()>1)
+		logPrintf("HINT: For gamma-point only calculations, turn off symmetries to speed up exact exchange.\n");
 	
-	//Initialize full k-mesh properties:
-	const Supercell& supercell = *(e.coulombParams.supercell);
-	kmesh.resize(supercell.kmesh.size());
-	for(unsigned ik=0; ik<kmesh.size(); ik++)
-	{	KmeshEntry& ki = kmesh[ik];
-		(Supercell::KmeshTransform&)ki = supercell.kmeshTransform[ik]; //copy base class over
-		ki.k = supercell.kmesh[ik];
+	//Initialize kmap:
+	logSuspend();
+	for(int iReduced=0; iReduced<qCount; iReduced++)
+	for(unsigned iInvert=0; iInvert<invertList.size(); iInvert++)
+	for(unsigned iSym=0; iSym<sym.size(); iSym++)
+	{	KmapEntry& ki = kmap[kmapIndex(iReduced, iInvert, iSym)];
+		ki.k = e.eInfo.qnums[iReduced].k * sym[iSym] * invertList[iInvert];
 		ki.basis.setup(e.gInfo, e.iInfo, e.cntrl.Ecut, ki.k);
-		if(e.eInfo.isMine(ki.iReduced) || e.eInfo.isMine(ki.iReduced + qCount))
-			ki.transform = std::make_shared<ColumnBundleTransform>(e.eInfo.qnums[ki.iReduced].k, e.basis[ki.iReduced],
-				ki.k, ki.basis, nSpinor, sym[ki.iSym], ki.invert);
+		if(e.eInfo.isMine(iReduced) || e.eInfo.isMine(iReduced + qCount))
+			ki.transform = std::make_shared<ColumnBundleTransform>(e.eInfo.qnums[iReduced].k, e.basis[iReduced],
+				ki.k, ki.basis, nSpinor, sym[iSym], invertList[iInvert]);
 	}
+	logResume();
 }
 
-double ExactExchangeEval::calc(int iSpin, int ik, double aXX, double omega, const std::vector<diagMatrix>& F, const std::vector<ColumnBundle>& C, std::vector<ColumnBundle>* HC) const
+double ExactExchangeEval::calc(int iSpin, unsigned iReduced, unsigned iInvert, unsigned iSym,
+	double aXX, double omega, const std::vector<diagMatrix>& F, const std::vector<ColumnBundle>& C, std::vector<ColumnBundle>* HC) const
 {
 	//Prepare ik state and gradient on all processes:
-	const KmeshEntry& ki = kmesh[ik];
+	const KmapEntry& ki = kmap[kmapIndex(iReduced, iInvert, iSym)];
+	int ikSrc = iReduced + iSpin*qCount; //source state number
 	const Basis& basis_k = ki.basis;
-	QuantumNumber qnum_k; qnum_k.k =  ki.k; qnum_k.spin = (nSpins==1 ? 0 : 1-2*iSpin); qnum_k.weight = 1./kmesh.size();
+	QuantumNumber qnum_k = e.eInfo.qnums[ikSrc]; qnum_k.k =  ki.k;
 	ColumnBundle Ck(e.eInfo.nBands, basis_k.nbasis*nSpinor, &basis_k, &qnum_k, isGpuEnabled()), HCk;
 	diagMatrix Fk(e.eInfo.nBands);
-	int ikSrc = ki.iReduced + iSpin*qCount; //source state number
 	if(e.eInfo.isMine(ikSrc))
 	{	Ck.zero();
 		ki.transform->scatterAxpy(1., C[ikSrc], Ck,0,1);
@@ -141,7 +152,7 @@ double ExactExchangeEval::calc(int iSpin, int ik, double aXX, double omega, cons
 	if(HC) { HCk = Ck.similar(); HCk.zero(); }
 	
 	//Calculate energy (and gradient):
-	const double prefac = -0.5*aXX;
+	const double prefac = -0.5*aXX / (sym.size()*invertList.size()*e.eInfo.spinWeight);
 	double EXX = 0.;
 	for(int bk=0; bk<e.eInfo.nBands; bk++)
 	{	//Put this state in real space:
@@ -189,7 +200,7 @@ double ExactExchangeEval::calc(int iSpin, int ik, double aXX, double omega, cons
 	if(HC)
 	{	HCk.allReduce(MPIUtil::ReduceSum);
 		if(e.eInfo.isMine(ikSrc))
-			ki.transform->gatherAxpy(qnum_k.weight/e.eInfo.qnums[ikSrc].weight, HCk,0,1, (*HC)[ikSrc]);
+			ki.transform->gatherAxpy(1., HCk,0,1, (*HC)[ikSrc]);
 	}
 	return EXX;
 }
