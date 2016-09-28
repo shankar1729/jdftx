d479f972cee0e7c4d78caa0b85d02eab678abf42
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2016-09-28 17:46:41 -0400

    Minor CMake tweaks for compilation on BlueGene/Q.
    Code now compiles with manually built GSL, FFTW, LAPACK and BLAS, but segfaults during command parsing. (Long way to go towards a successful port.)

--- jdftx/CMake-Modules/FindLAPACK_ATLAS.cmake
+++ jdftx/CMake-Modules/FindLAPACK_ATLAS.cmake
@@ -1,20 +1,26 @@
 #Wrapper to system FindLAPACK.cmake that handles new ATLAS library format correctly
 
-#Try to find the ATLAS sequential library:
-find_library(ATLAS_LIBRARY NAMES satlas PATHS ${ATLAS_PATH} ${ATLAS_PATH}/lib ${ATLAS_PATH}/lib64 /usr/lib64/atlas /usr/lib/atlas NO_DEFAULT_PATH)
-find_library(ATLAS_LIBRARY NAMES satlas)
-
-if(ATLAS_LIBRARY)
-	set(LAPACK_LIBRARIES ${ATLAS_LIBRARY})
-	set(LAPACK_FOUND TRUE)
-	if(NOT LAPACK_FIND_QUIETLY)
-		message(STATUS "Found LAPACK: ${LAPACK_LIBRARIES}")
-	endif()
+if(LAPACK_LIBRARIES)
+	#Externally specified LAPACK library list (with dependencies such as BLAS)
+	message(STATUS "Found LAPACK: ${LAPACK_LIBRARIES}")
 else()
-	#If not found, invoke the system FindLAPACK:
-	if(LAPACK_ATLAS_REQUIRED)
-		find_package(LAPACK REQUIRED)
+	#Try to find the ATLAS sequential library:
+	find_library(ATLAS_LIBRARY NAMES satlas PATHS ${ATLAS_PATH} ${ATLAS_PATH}/lib ${ATLAS_PATH}/lib64 /usr/lib64/atlas /usr/lib/atlas NO_DEFAULT_PATH)
+	find_library(ATLAS_LIBRARY NAMES satlas)
+
+	if(ATLAS_LIBRARY)
+		set(LAPACK_LIBRARIES ${ATLAS_LIBRARY})
+		set(LAPACK_FOUND TRUE)
+		if(NOT LAPACK_FIND_QUIETLY)
+			message(STATUS "Found LAPACK: ${LAPACK_LIBRARIES}")
+		endif()
 	else()
-		find_package(LAPACK)
+		#If not found, invoke the system FindLAPACK:
+		if(LAPACK_ATLAS_FIND_REQUIRED)
+			find_package(LAPACK REQUIRED)
+		else()
+			find_package(LAPACK)
+		endif()
 	endif()
 endif()
+
