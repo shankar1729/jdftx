2697d58ce937409d09bdfb370cee54475da14a0c
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2017-12-19 19:46:36 -0500

    Updated GBRV psps, PBEsol GBRV psps and links to latest version of each GBRV/SG15 psp

--- jdftx/pseudopotentials/update.sh
+++ jdftx/pseudopotentials/update.sh
@@ -17,6 +17,39 @@ function processSet()
 }
 
 processSet GBRV
+processSet GBRV-2017
 processSet SG15
 processSet SG15-pulay
 
+
+#Add links to latest GBRV pseudopotentials:
+cd $DEST_DIR/GBRV
+prefixes=$( for file in *_*_*.uspp; do
+		basename $file | awk '{split($1,tokens,"_v"); print tokens[1]}'
+	done | sort | uniq )
+for prefix in $prefixes; do
+	for version in "1" "1.01" "1.2" "1.4" "1.5"; do
+		fname="${prefix}_v${version}.uspp"
+		if [ -f $fname ]; then
+			latestVersion="$fname"
+		fi
+	done
+	ln -sf $latestVersion ${prefix}.uspp
+done
+
+#Add links to latest SG15 pseudopotentials:
+cd $DEST_DIR/SG15
+prefixes=$( for file in *_*_*-*.upf; do
+		basename $file | awk '{split($1,tokens,"-"); print tokens[1]}'
+	done | sort | uniq )
+for prefix in $prefixes; do
+	for suffix in "upf" "pulay"; do
+		for version in "1.0" "1.1"; do
+			fname="${prefix}-${version}.${suffix}"
+			if [ -f $fname ]; then
+				latestVersion="$fname"
+			fi
+		done
+		ln -sf $latestVersion ${prefix}.${suffix}
+	done
+done
