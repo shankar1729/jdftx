62b7f4571e63473c619ee2edf9e37b69e4984dc3
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2017-12-20 11:02:25 -0500

    Updated some tests to use LDA and PBEsol

--- jdftx/test/metalSurface/checkResults.sh
+++ jdftx/test/metalSurface/checkResults.sh
@@ -2,7 +2,7 @@
 
 echo "4"  #number of checks
 
-awk '/IonicMinimize: Iter/ { E = $5 } END { print E, "-266.8440 0.0001 Neutral Pt(111) energy [Eh]" }' neutral.out
-awk '/FillingsUpdate/ { mu = $3 } END { print mu, "-0.196 0.001 Neutral Pt(111) mu [Eh]" }' neutral.out
-awk '/IonicMinimize: Iter/ { E = $5 } END { print E, "-259.1668 0.0001 Fixed-mu Pt(111) energy [Eh]" }' charged.out
-awk '/FillingsUpdate/ { Q = $NF - 48 } END { print Q, "0.211 0.001 Fixed-mu Pt(111) charge [e]" }' charged.out
+awk '/IonicMinimize: Iter/ { E = $5 } END { print E, "-267.0675 0.0001 Neutral Pt(111) energy [Eh]" }' neutral.out
+awk '/FillingsUpdate/ { mu = $3 } END { print mu, "-0.202 0.001 Neutral Pt(111) mu [Eh]" }' neutral.out
+awk '/IonicMinimize: Iter/ { E = $5 } END { print E, "-259.3916 0.0001 Fixed-mu Pt(111) energy [Eh]" }' charged.out
+awk '/FillingsUpdate/ { Q = $NF - 48 } END { print Q, "0.244 0.001 Fixed-mu Pt(111) charge [e]" }' charged.out
