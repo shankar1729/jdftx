aec2475529d9b044551a68f6b8d7e59da4efd06e
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2019-06-11 17:17:29 -0400

    Moved initial rotations / loading from saveMLWF to new function initRotations() in WannierMinimizer_init.cpp

--- jdftx/wannier/WannierMinimizer.h
+++ jdftx/wannier/WannierMinimizer.h
@@ -152,25 +152,30 @@ protected:
 	//! Gradient propagation corresponding to axpyWfns: from dOmega/d(result) to dOmega/dA
 	void axpyWfns_grad(double alpha, matrix& Omega_A, const Kpoint& kpoint, int iSpin, const ColumnBundle& Omega_result) const;
 	
-	//! Get the trial wavefunctions (hydrogenic, atomic or numerical orbitals) for the group of centers in the common basis
-	ColumnBundle trialWfns(const Kpoint& kpoint) const;
-	std::map< Kpoint, std::shared_ptr<ColumnBundle> > numericalOrbitals; //!< numerical orbitals read from file
-	
 	//! Overlap between columnbundles of different k-points, with appropriate ultrasoft augmentation.
 	//! Note that the augmentation in the O() from electronic/ColumnBundle.h assumes both sides have same k-point.
 	//! If provided, use the cached projections instead of recomputing them.
 	matrix overlap(const ColumnBundle& C1, const ColumnBundle& C2, const std::vector<matrix>* VdagC1ptr=0, const std::vector<matrix>* VdagC2ptr=0) const;
 	
-	//! Wannierize and dump a Bloch-space matrix to file, optionally zeroing out the real parts
-	void dumpWannierized(const matrix& Htilde, const std::map<vector3<int>,matrix>& iCellMap,
-		const matrix& phase, int nMatrices, string varName, bool realPartOnly, int iSpin) const;
+	//! Preconditioner for Wannier optimization: identity by default, override in derived class to change
+	virtual WannierGradient precondition(const WannierGradient& grad);
+
+private:
+
+	//! Get the trial wavefunctions (hydrogenic, atomic or numerical orbitals) for the group of centers in the common basis
+	ColumnBundle trialWfns(const Kpoint& kpoint) const;
+	std::map< Kpoint, std::shared_ptr<ColumnBundle> > numericalOrbitals; //!< numerical orbitals read from file
 	
 	//! Return an exactly unitary version of U (orthogonalize columns)
 	//! If isSingular is provided, function will set it to true and return rather than stack-tracing in singular cases.
 	static matrix fixUnitary(const matrix& U, bool* isSingular=0); 
 	
-	//! Preconditioner for Wannier optimization: identity by default, override in derived class to change
-	virtual WannierGradient precondition(const WannierGradient& grad);
+	//! Load / compute rotations for a given spin channel (used by saveMLWF)
+	void initRotations(int iSpin);
+	
+	//! Wannierize and dump a Bloch-space matrix to file, optionally zeroing out the real parts
+	void dumpWannierized(const matrix& Htilde, const std::map<vector3<int>,matrix>& iCellMap,
+		const matrix& phase, int nMatrices, string varName, bool realPartOnly, int iSpin) const;
 };
 
 //! @}
