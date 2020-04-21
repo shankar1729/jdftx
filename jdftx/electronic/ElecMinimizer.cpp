250d8f1e59aae98051a7b372091058f08b9e659e
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2020-04-21 18:37:02 -0400

    Removed bypass of setEigenvectors() in IonicDynamics, required for correct stresses

--- jdftx/electronic/ElecMinimizer.cpp
+++ jdftx/electronic/ElecMinimizer.cpp
@@ -363,7 +363,7 @@ void elecMinimize(Everything& e)
 	else
 	{	ElecMinimizer emin(e);
 		emin.minimize(e.elecMinParams);
-		if (!e.ionicDynParams.nSteps) e.eVars.setEigenvectors(); //Don't spend time with this if running MD
+		e.eVars.setEigenvectors();
 	}
 	e.eVars.isRandom = false; //wavefunctions are no longer random
 	//Converge empty states if necessary:
