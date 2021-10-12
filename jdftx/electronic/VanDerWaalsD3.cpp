919510ad2e33c64d6698f2a50906fefc4647c62f
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2021-10-12 09:40:51 -0400

    Updated LJ override in DFT-D3 to keep original damping form instead of true LJ

--- jdftx/electronic/VanDerWaalsD3.cpp
+++ jdftx/electronic/VanDerWaalsD3.cpp
@@ -124,17 +124,10 @@ double VanDerWaalsD3::getScaleFactor(string exCorrName, double scaleOverride) co
 
 //! Return r^-n pair-potential energy and set its derivative E_r.
 //! The potential is damped at length scale R0 and exponent alpha_n.
-//! If ljOverride, return a standard LJ form for n=6, but keep the DFT-D3 damping form for all other n.
-template<int n, int alpha_n> double vdWpotential(double invr, double R0, double& E_r, bool ljOverride)
+template<int n, int alpha_n> double vdWpotential(double invr, double R0, double& E_r)
 {	//Main r^-n potential (and r derivative):
 	double pot = std::pow(invr, n); 
 	double pot_r = (-n) * pot * invr;
-	//Optional LJ override for MD/relaxation testing:
-	if(ljOverride and (n == 6))
-	{	double R06 = std::pow(R0, 6);
-		E_r = (1. - R06*pot) * pot_r;
-		return pot * (1. - 0.5*R06*pot);
-	}
 	//Exponent in denominator of damping factor (and r derivative):
 	double damp_term = 6 * std::pow(R0 * invr, alpha_n);
 	double damp_term_r = (-alpha_n) * damp_term * invr;
@@ -189,8 +182,8 @@ double VanDerWaalsD3::energyAndGrad(std::vector<Atom>& atoms, const double scale
 				{	double r = sqrt(rSq);
 					double invr = 1./r;
 					double cellWeight = (iR[2] ? 1. : 0.5); //account for double-counting in half-space cut plane
-					double term6_r; double term6 = (vdWpotential<6, D3::alpha6>(invr, sr6 * pp.R0, term6_r, e.iInfo.ljOverride));
-					double term8_r; double term8 = (vdWpotential<8, D3::alpha8>(invr, sr8 * pp.R0, term8_r, e.iInfo.ljOverride));
+					double term6_r; double term6 = (vdWpotential<6, D3::alpha6>(invr, sr6 * pp.R0, term6_r));
+					double term8_r; double term8 = (vdWpotential<8, D3::alpha8>(invr, sr8 * pp.R0, term8_r));
 					E12_C6 -= cellWeight * s6 * term6;
 					E12_C8 -= cellWeight * s8 * term8;
 					//Colect forces and/or stresses:
