66792a75fc6e2bba45238a018c1f86cb10c75764
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2022-07-12 09:52:11 -0400

    Fixed missing overlap operator and lightly cleaned-up Berry code.

--- jdftx/electronic/DumpCprime.cpp
+++ jdftx/electronic/DumpCprime.cpp
@@ -33,33 +33,35 @@ DumpCprime::DumpCprime(double dk, double degeneracyThreshold, double vThreshold,
 void DumpCprime::dump(Everything& e) const
 {
 	logPrintf("\n---- dC/dk calculation with dk = %lg a0^-1 and degeneracyThreshold = %le Eh ----\n", dk, degeneracyThreshold);
+	bool needBerry = e.dump.count(std::make_pair(DumpFreq_End, DumpBerry));
 	bool needL = e.dump.count(std::make_pair(DumpFreq_End, DumpL));
 	bool needQ = e.dump.count(std::make_pair(DumpFreq_End, DumpQ));
-	bool needBerry = e.dump.count(std::make_pair(DumpFreq_End, DumpBerry));
 	int nBands = e.eInfo.nBands;
 	
 	//Compute dC/dk, Berry Curvature Omega_k = i <dC/dk| X |dC/dk>, r*[r,H] moments and hence L or Q moments for each reduced k:
 	std::vector<matrix> L(e.eInfo.nStates), Q(e.eInfo.nStates), BerryCurvature(e.eInfo.nStates);
 	for(int q=e.eInfo.qStart; q<e.eInfo.qStop; q++)
-	{	//Compute dC/dk and r*[r,H] moments using dC/dk:
-		ColumnBundle Cprime_i[3]; matrix3<matrix> rrH;
+	{	//Compute dC/dk, and r*[r,H] moments using dC/dk if needed:
+		ColumnBundle Cprime[3];
+		matrix3<matrix> rrH;
 		for(int iDir=0; iDir<3; iDir++)
 		{	matrix CprimeOC;
-			Cprime_i[iDir] = getCprime(e, q, iDir, CprimeOC); //Compute idC/dk (effectively r*C)
-			if (needL || needQ){
-				matrix CprimeHC = CprimeOC * e.eVars.Hsub_eigs[q]; //since C are eigenvectors
-				rrH.set_row(iDir, e.iInfo.rHcommutator(Cprime_i[iDir], e.eVars.C[q], CprimeHC)); //effectively (r*C) ^ p . C
+			ColumnBundle Cprime_i = getCprime(e, q, iDir, CprimeOC); //Compute idC/dk (effectively r*C)
+			if(needBerry) Cprime[iDir] = Cprime_i; //only store if cross-terms needed (eg. for Berry curvature)
+			if(needL or needQ)
+			{	matrix CprimeHC = CprimeOC * e.eVars.Hsub_eigs[q]; //since C are eigenvectors
+				rrH.set_row(iDir, e.iInfo.rHcommutator(Cprime_i, e.eVars.C[q], CprimeHC)); //effectively (r*C) ^ p . C
 			}
 		}
-		if (needBerry){
-			BerryCurvature[q] = zeroes(nBands, nBands * 3);
-			const ColumnBundle Y[3] = { Cprime_i[0], Cprime_i[1], Cprime_i[2] };
-			for (int kDir = 0; kDir < 3; kDir++){
-				int iDir = (kDir + 1) % 3;
+		//Compute Berry curvature if needed:
+		if (needBerry)
+		{	BerryCurvature[q] = zeroes(nBands, nBands * 3);
+			for (int kDir = 0; kDir < 3; kDir++)
+			{	int iDir = (kDir + 1) % 3;
 				int jDir = (kDir + 2) % 3;
-				// BerryCurvature = i <dC/dk| X |dC/dk> = i <idC/dk| X |idC/dk>
-				matrix BerryCurvatureqk = complex(0, 1) * ((Y[iDir] ^ Y[jDir]) - (Y[jDir] ^ Y[iDir]));
-				BerryCurvature[q].set(0, nBands, kDir*nBands, (kDir + 1)*e.eInfo.nBands, BerryCurvatureqk);
+				matrix BerryCurvature_qk = complex(0, 1) * (Cprime[iDir] ^ O(Cprime[jDir])); // i <dC/dki | dC/dkj>
+				BerryCurvature_qk += dagger(BerryCurvature_qk); // -i <dC/dkj | dC/dki>
+				BerryCurvature[q].set(0, nBands, kDir*nBands, (kDir + 1)*e.eInfo.nBands, BerryCurvature_qk);
 			}
 		}
 		//Extract L if needed:
@@ -90,7 +92,7 @@ void DumpCprime::dump(Everything& e) const
 	}
 	logPrintf("\n---- dC/dk complete ----\n\n"); logFlush();
 
-	//Output L if requested:
+	//Output Berry curvature if requested:
 	if (needBerry)
 	{	string fname = e.dump.getFilename("BerryCurvature");
 		logPrintf("Dumping '%s' ... ", fname.c_str()); logFlush();
@@ -98,6 +100,7 @@ void DumpCprime::dump(Everything& e) const
 		logPrintf("done.\n");
 	}
 
+	//Output L if requested:
 	if(needL)
 	{	string fname = e.dump.getFilename("L");
 		logPrintf("Dumping '%s' ... ", fname.c_str()); logFlush();
